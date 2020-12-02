3c7ad778286bb8237a61ca9f9e3991bf5fa7ccb3
StevenMeara <stevenmeara2@gmail.com>
2020-12-02 13:31:45 -0500

    1.1

---
+++ build/precache-manifest.75d72ce729f990908ff8cb150f5a3562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810cd1389801ee9c836f2312aaaefcae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a570250f524840b84fc",
+    "url": "/static/css/main.29365b0a.chunk.css"
+  },
+  {
+    "revision": "56287bd0ac20e86351cd",
+    "url": "/static/js/2.99d52ba2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.99d52ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a570250f524840b84fc",
+    "url": "/static/js/main.d5c29554.chunk.js"
+  },
+  {
+    "revision": "64fcb5cd0d39d2c6c925",
+    "url": "/static/js/runtime-main.b587a6e9.js"
+  }
+]);
